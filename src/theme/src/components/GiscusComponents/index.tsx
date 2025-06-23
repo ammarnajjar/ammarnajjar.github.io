bb109cd6cf18b2cc52d65514ccb42cc94567189f
Ammar Najjar <najjarammar@gmail.com>
2025-06-23 11:44:05 +0200

    add giscus comments

---
+++ src/theme/src/components/GiscusComponents/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Giscus from "@giscus/react";
+import { useColorMode } from "@docusaurus/theme-common";
+
+export default function GiscusComponent() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Giscus
+      repo="ammarnajjar/github.io-comments"
+      repoId="R_kgDOJA1LSA"
+      category="[ENTER CATEGORY NAME HERE]"
+      categoryId="[ENTER CATEGORY ID HERE]"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="1"
+      inputPosition="top"
+      theme={colorMode}
+      lang="en"
+      loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  );
+}
