64821c603e3399fb969f398676d42313c7daa4be
Ammar Najjar <najjarammar@gmail.com>
2026-01-10 20:30:47 +0100

    Add favicon to enhance branding and user experience

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,6 +8,8 @@ const config: Config = {
   title: "Ammar Najjar",
   tagline: "Personal Website",
 
+  favicon: "img/favicon.ico",
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
